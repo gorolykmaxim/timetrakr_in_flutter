25d484616303784c65ac8de7c28c47720953f4b2
gorolykmaxim <streetbattle52@gmail.com>
2020-01-02 21:54:44 +0200

    Migrate to newer version of flutter_repositry

--- test/src/common.dart
+++ test/src/common.dart
@@ -1,9 +1,6 @@
 import 'package:flutter_event_projections/flutter_event_projections.dart';
-import 'package:flutter_repository/flutter_repository.dart';
 import 'package:mockito/mockito.dart';
 import 'package:timetrakr_in_flutter/src/query.dart';
-
-class SimpleCollectionMock<T> extends Mock implements SimpleCollection<T> {}
 
 class ObservableEventStreamMock<T> extends Mock implements ObservableEventStream<T> {}
 
